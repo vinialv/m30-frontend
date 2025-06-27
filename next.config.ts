21e50f3c39a6cb11e2a6f51861a7f6b2c182da0d
Vinicius Alves <vini.alv15@hotmail.com>
2025-06-26 22:50:35 -0300

    Setup inicial do projeto com dependências e configurações básicas

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: process.env.NEXT_PUBLIC_IMAGE_PROTOCOL as 'http' | 'https' | undefined,
+        hostname: process.env.NEXT_PUBLIC_IMAGE_HOSTNAME || 'localhost',
+        port: process.env.NEXT_PUBLIC_IMAGE_PORT,
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
